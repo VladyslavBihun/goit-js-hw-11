468660674eadd5f0ed2d939260bc795adc25b972
VladyslavBihun <bihunvv@gmail.com>
2024-04-10 22:02:35 +0300

    almost done

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-
 import { createMarkup } from './js/render-functions';
 import { searchImage } from './js/pixabay-api';
 
 const form = document.querySelector(".form");
 const input = document.querySelector(".input");
-const button = document.querySelector(".button");
 const list = document.querySelector(".list");
 const loader = document.querySelector(".loader")
 
@@ -40,17 +38,12 @@ function handleSubmit(event) {
             loader.style.display = 'none';
             form.reset();
         });
-}
+};
 
+let gallery = new SimpleLightbox('.list a', { 
+    sourceAttr: "href",
+    captionsData: "alt",
+    captionDelay: 250
+ });
 
-
-// iziToast.error({
-//         message: `Sorry, there are no images matching your search query. Please try again!`,
-//         theme: 'dark',
-//         messageColor: '#fff',
-//         messageSize: '16px',
-//         messageLineHeight: '1.5',
-//         backgroundColor: '#EF4040',
-//         position: 'topRight',
-//         progressBarColor: '#B51B1B',
-//     });
+gallery.refresh();
